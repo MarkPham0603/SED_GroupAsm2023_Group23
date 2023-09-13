a56d9d67eed360980a660bcd35dda2c39d90d694
QuangHuyTrinh <111732883+QuangHuyTrinh@users.noreply.github.com>
2023-09-13 04:58:33 +0000

    add "Shop" class & "Utility" class

--- main.cpp
+++ main.cpp
@@ -3,7 +3,44 @@
 #include <iomanip>
 using namespace std;
 
+namespace Shop
+{
+    class Shop
+    {
+    private:
+        string name;
+
+    public:
+        // constructor
+        Shop(string name = "") : name(name){};
+        // deconstructor
+        ~Shop()
+        {
+            name.clear();
+        }
+        void mainMenu();
+        void guestMenu();
+        void memberMenu();
+        void adminMenu();
+        void displayWelcomeScreen();
+    };
+
+}
+
+namespace utility
+{
+    class utility
+    {
+    public:
+        void saveDataToFile(string file_name);
+        void loadDataFromFile(string file_name);
+        void checkInput(string input);
+        void error_handling();
+    };
+}
+
 int main()
 {
+
     return 0;
 }
