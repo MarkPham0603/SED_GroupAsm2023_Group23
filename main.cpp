35c4077ed0f256533d64960319c419b2e1ade6aa
QuangHuyTrinh <s3970818@rmit.edu.vn>
2023-09-13 13:58:56 +0700

    add memberMenu and adjust mainMenu

--- main.cpp
+++ main.cpp
@@ -134,7 +134,7 @@ namespace Shop
         void displayWelcomeScreen();
     };
 
-    void Shop::displayWelcomeScreen() {
+    void Shop::mainMenu() {
         cout << "============================================================" << endl;
         cout << "EEET2482/COSC2082 GROUP ASSIGNMENT" << endl;
         cout << "MOTORBIKE RENTAL APPLICATION" << endl;
@@ -144,6 +144,7 @@ namespace Shop
         cout << "s3929256, Student Name: Pham Minh Hoa" << endl;
         cout << "s3938309, Student Name: Nguyen Dinh Quoc Bao" << endl;
         cout << "============================================================" << endl;
+        cout << "============================================================" << endl;
         cout << "Sign in as: " << endl;
         cout << "1. Member" << endl;
         cout << "2. Admin" << endl;
@@ -151,14 +152,17 @@ namespace Shop
         cout << "\nPlease enter your choice: ";
     }
 
-    void Shop::mainMenu() {
+    void Shop::memberMenu() {
+        cout << "============================================================" << endl;
+        cout << "Enter your username:";
+        cout << "Enter your password:";
+
+    }
+
+        void Shop::displayWelcomeScreen() {
         cout << "============================================================" << endl;
         cout << "WELCOME TO " << name << " MOTORBIKE RENTAL SHOP" << endl;
         cout << "============================================================" << endl;
-    }
-
-    void Shop::guestMenu() {
-        cout << 
     }
 }
 
