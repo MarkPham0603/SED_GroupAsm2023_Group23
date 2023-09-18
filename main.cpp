1fdc051be6ac167b41cdff174145bd1a7f3a78dd
QuangHuyTrinh <s3970818@rmit.edu.vn>
2023-09-18 22:00:39 +0700

    move list of member and motorbikes to admin

--- main.cpp
+++ main.cpp
@@ -17,27 +17,40 @@ namespace Member
 
     public:
         // Guest class constructor
-        Guest(const string &fullName, const string &phoneNumber,
-              const string &idType, const string &idPassportNumber,
-              const string &driverLicenseNumber, const string &expiryDate)
+        Guest(string fullName = "", string phoneNumber = "",
+              string idType = "", string idPassportNumber = "",
+              string driverLicenseNumber = "", string expiryDate = "")
             : fullName(fullName), phoneNumber(phoneNumber),
               idType(idType), idPassportNumber(idPassportNumber),
               driverLicenseNumber(driverLicenseNumber), expiryDate(expiryDate){};
 
-        Member registerAsMember()
+        void registerAsMember(Admin admin)
         {
             string username;
 
-            cout << "Enter Username: ";
-            cin >> username;
+            cout << "Set your username: ";
+            getline(cin, username);
+            cout << "Enter full name: ";
+            getline(cin, fullName);
+            cout << "Enter phone number: ";
+            getline(cin, phoneNumber);
+            cout << "Enter type of id: ";
+            getline(cin, idType);
+            cout << "Enter id number: ";
+            getline(cin, idPassportNumber);
+            cout << "Enter driver license number: ";
+            getline(cin, driverLicenseNumber);
+            cout << "Enter expiry date for driver license: ";
+            getline(cin, expiryDate);
 
             Member newMember(username, fullName, phoneNumber, idType, idPassportNumber, driverLicenseNumber, expiryDate);
+            admin.addMemberToList(newMember);
             cout << "Registration successful. Welcome, " << username << "!" << endl;
-            return newMember;
         }
 
-        void viewAllMotorbikeForRent(Shop::Shop shop){
-            shop.viewAllMotorbikeForRent();
+        void viewAllMotorbikeForGuest(Admin admin)
+        {
+            admin.viewAllMotorbikeForGuest();
         }
 
         friend Member;
@@ -59,7 +72,7 @@ namespace Member
                const string &username, int creditPoints = 20)
             : Guest(fullName, phoneNumber, idType, idPassportNumber, driverLicenseNumber, expiryDate),
               username(username), creditPoints(20), motorbike(motorbike), hasMotorbike(false){};
-    
+
         void viewInformation()
         {
             cout << "Username: " << username << endl;
@@ -73,164 +86,71 @@ namespace Member
             motorbike.viewmotorInfo();
         }
 
-        void listMotorbikeForRent() {
-                if (!hasMotorbike) {
-                cout << "You don't have a motorbike to list for rent." << endl;
-            } else {
-                cout << "Your Motorbike for Rent:" << endl;
-                motorbike.viewmotorInfo();
-            }
-        }
-
-        void unlistMotorbike()
+        void listMotorbikeForRent()
         {
-            if (!hasMotorbike) {
-                cout << "You don't have a motorbike listed for rent." << endl;
-            } else {
-                cout << "Unlisting your Motorbike for Rent:" << endl;
-                motorbike.viewmotorInfo(); // Display the details of the motorbike being unlisted
-                hasMotorbike = false; // Mark that the member no longer has a listed motorbike
-                cout << "Your motorbike has been unlisted." << endl;
-            }
-        }   
-    };
-    /*class Member
-    {
-    private:
-        string username;
-        string fullName;
-        string phoneNumber;
-        string idType;
-        string idPassportNumber;
-        string driverLicenseNumber;
-        string expiryDate;
-        int creditPoints;
-        Motorbike::Motorbike motorbike;
-        bool hasMotorbike;
-
-    public:
-        // Member constructor
-        Member(const string &username, const string &fullName, const string &phoneNumber,
-               const string &idType, const string &idPassportNumber,
-               const string &driverLicenseNumber, const string &expiryDate)
-            : username(username), fullName(fullName), phoneNumber(phoneNumber),
-              idType(idType), idPassportNumber(idPassportNumber),
-              driverLicenseNumber(driverLicenseNumber), expiryDate(expiryDate), creditPoints(20), motorbike(motorbike),hasMotorbike(false)
-        {
-        }
-
-        // Member view account and personal information
-        void viewInformation()
-        {
-            cout << "Username: " << username << endl;
-            cout << "Full Name: " << fullName << endl;
-            cout << "Phone Number: " << phoneNumber << endl;
-            cout << "ID Type: " << idType << endl;
-            cout << "ID/Passport Number: " << idPassportNumber << endl;
-            cout << "Driver's License Number: " << driverLicenseNumber << endl;
-            cout << "Expiry Date: " << expiryDate << endl;
-            cout << "Credit Points: " << creditPoints << endl;
-            motorbike.viewmotorInfo();
-        }
-
-        // Show the motor list of the user
-        void listMotorbikeForRent() {
-                if (!hasMotorbike) {
-                cout << "You don't have a motorbike to list for rent." << endl;
-            } else {
-                cout << "Your Motorbike for Rent:" << endl;
-                motorbike.viewmotorInfo();
-            }
-        }
-
-        // Hide the motor list of the user
-        void unlistMotorbike()
-        {
-            if (!hasMotorbike) {
-                cout << "You don't have a motorbike listed for rent." << endl;
-            } else {
-                cout << "Unlisting your Motorbike for Rent:" << endl;
-                motorbike.viewmotorInfo(); // Display the details of the motorbike being unlisted
-                hasMotorbike = false; // Mark that the member no longer has a listed motorbike
-                cout << "Your motorbike has been unlisted." << endl;
-            }
-        }
-
-        void requestToRentMotorbike()
-        {
-        }
-
-        void rateMotorbike()
-        {
-        }
-
-        void rateRenter()
-        {
-        }
-
-        // Static method for member registration
-        static Member registerAsMember()
-        {
-            string username, fullName, phoneNumber, idType, idPassportNumber, driverLicenseNumber, expiryDate;
-
-
-            cout << "Enter Username: ";
-            cin >> username;
-            cout << "Enter Full Name: ";
-            cin.ignore();
-            getline(cin, fullName);
-            cout << "Enter Phone Number: ";
-            cin >> phoneNumber;
-            cout << "Enter ID Type: ";
-            cin >> idType;
-            cout << "Enter ID/Passport Number: ";
-            cin >> idPassportNumber;
-            cout << "Enter Driver's License Number: ";
-            cin >> driverLicenseNumber;
-            cout << "Enter Expiry Date: ";
-            cin >> expiryDate;
-
-
-            Member newMember(username, fullName, phoneNumber, idType, idPassportNumber, driverLicenseNumber, expiryDate);
-            cout << "Registration successful. Welcome, " << username << "!" << endl;
-            return newMember;
-        }
-
-        bool login(const string &enteredUsername)
-        {
-            if (enteredUsername == username)
+            if (!hasMotorbike)
             {
-                cout << "Login successful. Welcome, " << username << "!" << endl;
-                return true;
+                cout << "You don't have a motorbike to list for rent." << endl;
             }
             else
             {
-                cout << "Login failed. Invalid username." << endl;
-                return false;
+                cout << "Your Motorbike for Rent:" << endl;
+                motorbike.viewmotorInfo();
+            }
+        }
+
+        void unlistMotorbike()
+        {
+            if (!hasMotorbike)
+            {
+                cout << "You don't have a motorbike listed for rent." << endl;
+            }
+            else
+            {
+                cout << "Unlisting your Motorbike for Rent:" << endl;
+                motorbike.viewmotorInfo(); // Display the details of the motorbike being unlisted
+                hasMotorbike = false;      // Mark that the member no longer has a listed motorbike
+                cout << "Your motorbike has been unlisted." << endl;
             }
         }
     };
 
-    class Admin {
+    class Admin
+    {
     private:
         string username;
         string password;
+        vector<Member> listOfMember;
+        vector<Motorbike::Motorbike> listOfMotorbikeForRent;
 
     public:
-        Admin(const string& adminUsername, const string& adminPassword)
-            : username(adminUsername), password(adminPassword) {}
+        Admin(const string adminUsername, string adminPassword)
+            : username(adminUsername), password(adminPassword){};
 
-        bool loginAsAdmin(const string& enteredUsername, const string& enteredPassword) {
-            if (enteredUsername == username && enteredPassword == password) {
-                cout << "Admin login successful. Welcome, " << username << "!" << endl;
-                return true;
-            } else {
-                cout << "Admin login failed. Invalid username or password." << endl;
-                return false;
+        void addMemberToList(Member &new_member)
+        {
+            listOfMember.push_back(new_member);
+        }
+        void addMotorbikeToList(Motorbike::Motorbike &new_motorbike)
+        {
+            listOfMotorbikeForRent.push_back(new_motorbike);
+        }
+
+        void viewAllMotorbikeForGuest()
+        {
+            if (listOfMotorbikeForRent.empty())
+            {
+                cout << "No motorbikes are available for rent at this time.\n";
+            }
+            else
+            {
+                for (size_t i = 0; i < listOfMotorbikeForRent.capacity(); i++)
+                {
+                    listOfMotorbikeForRent[i].viewmotorInfo();
+                }
             }
         }
     };
-    */
 }
 
 namespace Shop
@@ -239,7 +159,7 @@ namespace Shop
     {
     private:
         string name;
-        vector<Motorbike::Motorbike> listOfMotorbikeForRent;
+
     public:
         // constructor
         Shop(string name = "") : name(name){};
@@ -254,6 +174,7 @@ namespace Shop
         void adminMenu();
         void displayWelcomeScreen();
         void viewAllMotorbikeForRent();
+        void viewAllMotorbikeForGuest();
     };
 
     void Shop::mainMenu()
